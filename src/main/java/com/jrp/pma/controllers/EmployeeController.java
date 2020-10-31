a33dd14a2a32753273f9bae41150507f265dff94
Lyoonu <lyoonu17@gmail.com>
2020-10-31 19:58:07 +0900

    First commit: data input and showing list

---
+++ src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.jrp.pma.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping
+	public String displayEmployees(Model model) {
+		
+		List<Employee> employees = empRepo.findAll();
+		model.addAttribute("employeesList", employees);
+		
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/new")
+	public String displayEmployeeForm(Model model) {
+		
+		Employee anEmployee = new Employee();
+		
+		model.addAttribute("employee", anEmployee); //th:object="${employee}"
+		
+		return "employees/new-employee";
+	}
+	
+	@PostMapping("/save")
+	public String createEmployee(Employee employee, Model model) {
+		// save to the database using an employee crud repository
+		empRepo.save(employee);
+		
+		return "redirect:/employees";
+		
+	}
+	
+}
